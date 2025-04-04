ce4ed6b504ddc7c0b8cac41b24df615c793ed0ab
Xcution <ghosh1k4@gmail.com>
2025-04-04 15:27:49 +0530

    152704042025

---
+++ Python/0025_Reverse_Nodes_in_k-Group.py
@@ -0,0 +1,67 @@
+# Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
+
+# k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+
+# You may not alter the values in the list's nodes, only nodes themselves may be changed.
+
+ 
+
+# Example 1:
+
+
+# Input: head = [1,2,3,4,5], k = 2
+# Output: [2,1,4,3,5]
+# Example 2:
+
+
+# Input: head = [1,2,3,4,5], k = 3
+# Output: [3,2,1,4,5]
+ 
+
+# Constraints:
+
+# The number of nodes in the list is n.
+# 1 <= k <= n <= 5000
+# 0 <= Node.val <= 1000
+ 
+
+# Follow-up: Can you solve the problem in O(1) extra memory space?
+
+# Author: Kaustav Ghosh
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseKGroup(self, head, k):
+        def get_kth_node(curr, k):
+            while curr and k > 0:
+                curr = curr.next
+                k -= 1
+            return curr
+
+        dummy = ListNode(0)
+        dummy.next = head
+        group_prev = dummy
+
+        while True:
+            kth = get_kth_node(group_prev, k)
+            if not kth:
+                break
+            group_next = kth.next
+
+            # Reverse group
+            prev, curr = kth.next, group_prev.next
+            while curr != group_next:
+                tmp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = tmp
+
+            # Reconnect reversed group
+            tmp = group_prev.next
+            group_prev.next = kth
+            group_prev = tmp
+
+        return dummy.next
