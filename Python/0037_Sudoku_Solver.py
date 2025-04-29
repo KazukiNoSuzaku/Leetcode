fa5b04ef64410ef2eac5abd3a0f5ceec325cf6da
Xcution <ghosh1k4@gmail.com>
2025-04-29 18:10:45 +0530

    181029042025

---
+++ Python/0037_Sudoku_Solver.py
@@ -0,0 +1,75 @@
+# Write a program to solve a Sudoku puzzle by filling the empty cells.
+
+# A sudoku solution must satisfy all of the following rules:
+
+# Each of the digits 1-9 must occur exactly once in each row.
+# Each of the digits 1-9 must occur exactly once in each column.
+# Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+# The '.' character indicates empty cells.
+
+ 
+
+# Example 1:
+
+
+# Input: board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+# Output: [["5","3","4","6","7","8","9","1","2"],["6","7","2","1","9","5","3","4","8"],["1","9","8","3","4","2","5","6","7"],["8","5","9","7","6","1","4","2","3"],["4","2","6","8","5","3","7","9","1"],["7","1","3","9","2","4","8","5","6"],["9","6","1","5","3","7","2","8","4"],["2","8","7","4","1","9","6","3","5"],["3","4","5","2","8","6","1","7","9"]]
+# Explanation: The input board is shown above and the only valid solution is shown below:
+
+
+ 
+
+# Constraints:
+
+# board.length == 9
+# board[i].length == 9
+# board[i][j] is a digit or '.'.
+# It is guaranteed that the input board has only one solution.
+
+# Author: Kaustav Ghosh
+
+class Solution(object):
+    def solveSudoku(self, board):
+        from collections import defaultdict
+
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+        empty = []
+
+        # Pre-fill sets and record empty cells
+        for r in range(9):
+            for c in range(9):
+                val = board[r][c]
+                if val == '.':
+                    empty.append((r, c))
+                else:
+                    rows[r].add(val)
+                    cols[c].add(val)
+                    boxes[(r // 3) * 3 + (c // 3)].add(val)
+
+        def backtrack(index):
+            if index == len(empty):
+                return True  # all cells filled
+
+            r, c = empty[index]
+            b = (r // 3) * 3 + (c // 3)
+            for num in map(str, range(1, 10)):
+                if num not in rows[r] and num not in cols[c] and num not in boxes[b]:
+                    board[r][c] = num
+                    rows[r].add(num)
+                    cols[c].add(num)
+                    boxes[b].add(num)
+
+                    if backtrack(index + 1):
+                        return True
+
+                    # backtrack
+                    board[r][c] = '.'
+                    rows[r].remove(num)
+                    cols[c].remove(num)
+                    boxes[b].remove(num)
+
+            return False
+
+        backtrack(0)
