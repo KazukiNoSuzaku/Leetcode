c03a2f3f90707892f062c70a2e6cc096f0d1021d
Xcution <ghosh1k4@gmail.com>
2025-06-19 13:04:09 +0530

    130419062025

---
+++ Python/0047_Permutations_II.py
@@ -0,0 +1,45 @@
+# Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,1,2]
+# Output:
+# [[1,1,2],
+#  [1,2,1],
+#  [2,1,1]]
+# Example 2:
+
+# Input: nums = [1,2,3]
+# Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 8
+# -10 <= nums[i] <= 10
+
+# Author: Kaustav Ghosh
+
+class Solution(object):
+    def permuteUnique(self, nums):
+        def backtrack(start):
+            if start == len(nums):
+                result.append(nums[:])
+                return
+            
+            seen = set()
+            for i in range(start, len(nums)):
+                if nums[i] in seen:
+                    continue
+                seen.add(nums[i])
+                nums[start], nums[i] = nums[i], nums[start]
+                backtrack(start + 1)
+                nums[start], nums[i] = nums[i], nums[start]
+
+        result = []
+        nums.sort()  # Sort to handle duplicates
+        backtrack(0)
+        return result
+        
