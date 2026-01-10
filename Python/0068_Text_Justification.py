4f460e3feb4b368ca533d2b1f900295d0c2a302c
Xcution <ghosh1k4@gmail.com>
2026-01-10 22:48:29 +0530

    224810012026

--- Python/0068_Text_Justification.py
+++ Python/0068_Text_Justification.py
@@ -56,3 +56,20 @@
 # words[i].length <= maxWidth
 
 # Author: Kaustav Ghosh
+
+class Solution(object):
+    def fullJustify(self, words, maxWidth):
+        res = []
+        curr_line = []
+        num_of_letters = 0
+
+        for word in words:
+            if num_of_letters + len(word) + len(curr_line) > maxWidth:
+                for i in range(maxWidth - num_of_letters):
+                    curr_line[i % (len(curr_line) - 1 or 1)] += ' '
+                res.append(''.join(curr_line))
+                curr_line, num_of_letters = [], 0
+            curr_line.append(word)
+            num_of_letters += len(word)
+
+        return res + [' '.join(curr_line).ljust(maxWidth)]
