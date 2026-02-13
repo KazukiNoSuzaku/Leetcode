46cf7d880086a91e8fb28d41fc9ab56ddd153f41
Xcution <ghosh1k4@gmail.com>
2026-02-13 14:36:53 +0530

    143613022026

---
+++ Python/0079_Word_Search.py
@@ -0,0 +1,60 @@
+# Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+
+# The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+ 
+
+# Example 1:
+
+
+# Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+# Output: true
+# Example 2:
+
+
+# Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+# Output: true
+# Example 3:
+
+
+# Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+# Output: false
+ 
+
+# Constraints:
+
+# m == board.length
+# n = board[i].length
+# 1 <= m, n <= 6
+# 1 <= word.length <= 15
+# board and word consists of only lowercase and uppercase English letters.
+ 
+
+# Follow up: Could you use search pruning to make your solution faster with a larger board?
+
+# Author: Kaustav Ghosh
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        m, n = len(board), len(board[0])
+        visited = [[False] * n for _ in range(m)]
+
+        def dfs(i, j, k):
+            if board[i][j] != word[k]:
+                return False
+            if k == len(word) - 1:
+                return True
+
+            visited[i][j] = True
+            for x, y in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
+                if 0 <= x < m and 0 <= y < n and not visited[x][y]:
+                    if dfs(x, y, k + 1):
+                        return True
+            visited[i][j] = False
+            return False
+
+        for i in range(m):
+            for j in range(n):
+                if dfs(i, j, 0):
+                    return True
+        return False
