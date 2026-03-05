bca86c8029df92dd8d5eb535a9e23cc5daa3e119
Xcution <ghosh1k4@gmail.com>
2026-03-05 20:33:22 +0530

    Add solution and README entry for problem 125

---
+++ Python/0125_Valid_Palindrome.py
@@ -0,0 +1,27 @@
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters
+# and removing all non-alphanumeric characters, it reads the same forward and backward.
+# Alphanumeric characters include letters and numbers.
+# Given a string s, return true if it is a palindrome, or false otherwise.
+
+# Example 1:
+# Input: s = "A man, a plan, a canal: Panama"
+# Output: true
+
+# Example 2:
+# Input: s = "race a car"
+# Output: false
+
+# Example 3:
+# Input: s = " "
+# Output: true
+
+# Constraints:
+# 1 <= s.length <= 2 * 10^5
+# s consists only of printable ASCII characters.
+
+# Author: Kaustav Ghosh
+
+class Solution(object):
+    def isPalindrome(self, s):
+        filtered = [c.lower() for c in s if c.isalnum()]
+        return filtered == filtered[::-1]
