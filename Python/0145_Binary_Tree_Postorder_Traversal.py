3159d7ae9b8beb85d8f9fb9b2910dfce6f220793
Xcution <ghosh1k4@gmail.com>
2026-03-05 20:40:09 +0530

    Add solution and README entry for problem 145

---
+++ Python/0145_Binary_Tree_Postorder_Traversal.py
@@ -0,0 +1,38 @@
+# Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+# Example 1:
+# Input: root = [1,null,2,3]
+# Output: [3,2,1]
+
+# Example 2:
+# Input: root = []
+# Output: []
+
+# Example 3:
+# Input: root = [1]
+# Output: [1]
+
+# Constraints:
+# The number of nodes in the tree is in the range [0, 100].
+# -100 <= Node.val <= 100
+
+# Author: Kaustav Ghosh
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def postorderTraversal(self, root):
+        res = []
+        stack = [root] if root else []
+        while stack:
+            node = stack.pop()
+            res.append(node.val)
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        return res[::-1]
