012d4f4348e9f9648d735f4299ced4cc03f49600
Xcution <ghosh1k4@gmail.com>
2026-03-07 08:22:23 +0530

    Add solution and README entry for problem 304

---
+++ Python/0304_Range_Sum_Query_2D_Immutable.py
@@ -0,0 +1,33 @@
+# Given a 2D matrix matrix, handle multiple queries of the following type:
+# Calculate the sum of the elements of matrix inside the rectangle defined by its upper left
+# corner (row1, col1) and lower right corner (row2, col2).
+# Implement the NumMatrix class:
+# - NumMatrix(int[][] matrix) Initializes the object with the integer matrix matrix.
+# - int sumRegion(int row1, int col1, int row2, int col2) Returns the sum of the elements
+#   of matrix inside the rectangle.
+
+# Example 1:
+# Input: ["NumMatrix","sumRegion","sumRegion","sumRegion"]
+#        [[[[3,0,1,4,2],[5,6,3,2,1],[1,2,0,1,5],[4,1,0,1,7],[1,0,3,0,5]]],[2,1,4,3],[1,1,2,2],[1,2,2,4]]
+# Output: [null,8,11,12]
+
+# Constraints:
+# m == matrix.length, n == matrix[i].length
+# 1 <= m, n <= 200
+# -10^4 <= matrix[i][j] <= 10^4
+
+# Author: Kaustav Ghosh
+
+class NumMatrix(object):
+    def __init__(self, matrix):
+        m, n = len(matrix), len(matrix[0])
+        self.pre = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                self.pre[i][j] = (matrix[i-1][j-1] + self.pre[i-1][j]
+                                  + self.pre[i][j-1] - self.pre[i-1][j-1])
+
+    def sumRegion(self, row1, col1, row2, col2):
+        r1, c1, r2, c2 = row1 + 1, col1 + 1, row2 + 1, col2 + 1
+        return (self.pre[r2][c2] - self.pre[r1-1][c2]
+                - self.pre[r2][c1-1] + self.pre[r1-1][c1-1])
