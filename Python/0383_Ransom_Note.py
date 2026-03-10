c869e6fb737182bb4d3052bac03eda9d81eb3b7c
Xcution <ghosh1k4@gmail.com>
2026-03-11 03:10:07 +0530

    Add solution and README entry for problem 383

---
+++ Python/0383_Ransom_Note.py
@@ -0,0 +1,32 @@
+# Given two strings ransomNote and magazine, return true if ransomNote can be constructed
+# by using the letters from magazine and false otherwise.
+# Each letter in magazine can only be used once in ransomNote.
+
+# Example 1:
+# Input: ransomNote = "a", magazine = "b"
+# Output: false
+
+# Example 2:
+# Input: ransomNote = "aa", magazine = "ab"
+# Output: false
+
+# Example 3:
+# Input: ransomNote = "aa", magazine = "aab"
+# Output: true
+
+# Constraints:
+# 1 <= ransomNote.length, magazine.length <= 10^5
+# ransomNote and magazine consist of lowercase English letters.
+
+# Author: Kaustav Ghosh
+
+from collections import Counter
+
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        mag_count = Counter(magazine)
+        for ch in ransomNote:
+            if mag_count[ch] <= 0:
+                return False
+            mag_count[ch] -= 1
+        return True
