cc9e40a471df22d4b93cdcec6f67afdafe7c653c
Xcution <ghosh1k4@gmail.com>
2026-03-17 00:56:32 +0530

    Add solution and README entry for problem 1389

---
+++ Python/1389_Create_Target_Array_in_the_Given_Order.py
@@ -0,0 +1,15 @@
+# Author: Kaustav Ghosh
+# Problem: Create Target Array in the Given Order
+# Approach: Simulate insertions at given indices
+
+class Solution(object):
+    def createTargetArray(self, nums, index):
+        """
+        :type nums: List[int]
+        :type index: List[int]
+        :rtype: List[int]
+        """
+        target = []
+        for n, i in zip(nums, index):
+            target.insert(i, n)
+        return target
