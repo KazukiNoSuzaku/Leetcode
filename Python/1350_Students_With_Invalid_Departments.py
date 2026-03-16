e6d0228cbd51ae3272344daad056291c4150fd76
Xcution <ghosh1k4@gmail.com>
2026-03-17 00:24:42 +0530

    Add solution and README entry for problem 1350

---
+++ Python/1350_Students_With_Invalid_Departments.py
@@ -0,0 +1,11 @@
+# Premium SQL problem - Find students enrolled in departments that no longer exist.
+
+-- Author: Kaustav Ghosh
+
+# SQL solution:
+# SELECT id, name
+# FROM Students
+# WHERE department_id NOT IN (SELECT id FROM Departments);
+
+class Solution(object):
+    pass
