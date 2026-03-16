b13647b94fa7c25c31a03ce0ebda16ad3986a7b5
Xcution <ghosh1k4@gmail.com>
2026-03-17 00:58:46 +0530

    Add solution and README entry for problem 1395

---
+++ Python/1395_Count_Number_of_Teams.py
@@ -0,0 +1,27 @@
+# Author: Kaustav Ghosh
+# Problem: Count Number of Teams
+# Approach: For each middle soldier count valid triples
+
+class Solution(object):
+    def numTeams(self, rating):
+        """
+        :type rating: List[int]
+        :rtype: int
+        """
+        n = len(rating)
+        count = 0
+        for j in range(n):
+            left_less = left_greater = 0
+            right_less = right_greater = 0
+            for i in range(j):
+                if rating[i] < rating[j]:
+                    left_less += 1
+                elif rating[i] > rating[j]:
+                    left_greater += 1
+            for k in range(j + 1, n):
+                if rating[k] < rating[j]:
+                    right_less += 1
+                elif rating[k] > rating[j]:
+                    right_greater += 1
+            count += left_less * right_greater + left_greater * right_less
+        return count
