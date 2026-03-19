6c6d8398a7f30df16ea32e97e5a9b0fba5942ced
Xcution <ghosh1k4@gmail.com>
2026-03-19 22:46:13 +0530

    Add solution and README entry for problem 1646

---
+++ Python/1646_Get_Maximum_in_Generated_Array.py
@@ -0,0 +1,19 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/get-maximum-in-generated-array/
+
+class Solution(object):
+    def getMaximumGenerated(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 0:
+            return 0
+        nums = [0] * (n + 1)
+        nums[1] = 1
+        for i in range(2, n + 1):
+            if i % 2 == 0:
+                nums[i] = nums[i // 2]
+            else:
+                nums[i] = nums[i // 2] + nums[i // 2 + 1]
+        return max(nums)
