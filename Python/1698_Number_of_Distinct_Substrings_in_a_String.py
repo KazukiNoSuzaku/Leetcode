8a414e1881d5db31a15d3979bdb095939facde06
Xcution <ghosh1k4@gmail.com>
2026-03-19 23:02:03 +0530

    Add solution and README entry for problem 1698

---
+++ Python/1698_Number_of_Distinct_Substrings_in_a_String.py
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/number-of-distinct-substrings-in-a-string/
+# Premium Problem
+# Use trie or suffix array to count distinct substrings
+
+class Solution(object):
+    pass
