54fa4868e3b0ecd91000070fe07fdd66697ce608
Xcution <ghosh1k4@gmail.com>
2026-03-19 22:59:20 +0530

    Add solution and README entry for problem 1688

---
+++ Python/1688_Count_of_Matches_in_Tournament.py
@@ -0,0 +1,10 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/count-of-matches-in-tournament/
+
+class Solution(object):
+    def numberOfMatches(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return n - 1
