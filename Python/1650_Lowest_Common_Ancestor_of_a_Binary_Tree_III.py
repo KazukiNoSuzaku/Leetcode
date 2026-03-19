4de7441466a08cab53a55ba80379b4fc2588a447
Xcution <ghosh1k4@gmail.com>
2026-03-19 22:47:37 +0530

    Add solution and README entry for problem 1650

---
+++ Python/1650_Lowest_Common_Ancestor_of_a_Binary_Tree_III.py
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree-iii/
+# Premium Problem
+# Two pointer approach: traverse to root from both nodes, swap at end
+
+class Solution(object):
+    pass
