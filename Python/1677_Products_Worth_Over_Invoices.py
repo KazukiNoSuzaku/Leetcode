06784960acb9f266116505c92853022e61b0d9dc
Xcution <ghosh1k4@gmail.com>
2026-03-19 22:56:16 +0530

    Add solution and README entry for problem 1677

---
+++ Python/1677_Products_Worth_Over_Invoices.py
@@ -0,0 +1,16 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/products-worth-over-invoices/
+# Premium SQL Problem
+#
+# SELECT p.name,
+#        IFNULL(SUM(i.rest), 0) AS rest,
+#        IFNULL(SUM(i.paid), 0) AS paid,
+#        IFNULL(SUM(i.canceled), 0) AS canceled,
+#        IFNULL(SUM(i.refunded), 0) AS refunded
+# FROM Product p
+# LEFT JOIN Invoice i ON p.product_id = i.product_id
+# GROUP BY p.name
+# ORDER BY p.name;
+
+class Solution(object):
+    pass
