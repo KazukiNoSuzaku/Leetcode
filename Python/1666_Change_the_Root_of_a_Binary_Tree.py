417736969199e7d3b5a80620b330b176824b28d4
Xcution <ghosh1k4@gmail.com>
2026-03-19 22:52:52 +0530

    Add solution and README entry for problem 1666

---
+++ Python/1666_Change_the_Root_of_a_Binary_Tree.py
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/change-the-root-of-a-binary-tree/
+# Premium Problem
+# Traverse from node to root reversing parent pointers
+
+class Solution(object):
+    pass
