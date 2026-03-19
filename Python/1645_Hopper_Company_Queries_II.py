933d8ec5bd9e6be3d754e360085110e492fbb623
Xcution <ghosh1k4@gmail.com>
2026-03-19 22:45:55 +0530

    Add solution and README entry for problem 1645

---
+++ Python/1645_Hopper_Company_Queries_II.py
@@ -0,0 +1,8 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/hopper-company-queries-ii/
+# Premium SQL Problem
+#
+# Similar to 1635 but compute percentage of working drivers who accepted rides
+
+class Solution(object):
+    pass
