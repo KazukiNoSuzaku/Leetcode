d06b8b7afa392899b7710d53581f01dd0f7de862
Xcution <ghosh1k4@gmail.com>
2026-03-19 22:39:24 +0530

    Add solution and README entry for problem 1623

---
+++ SQL/1623_All_Valid_Triplets_That_Can_Represent_a_Country.sql
@@ -0,0 +1,11 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/all-valid-triplets-that-can-represent-a-country/
+
+SELECT
+    a.student_name AS member_A,
+    b.student_name AS member_B,
+    c.student_name AS member_C
+FROM SchoolA a
+JOIN SchoolB b ON a.student_id != b.student_id AND a.student_name != b.student_name
+JOIN SchoolC c ON a.student_id != c.student_id AND b.student_id != c.student_id
+    AND a.student_name != c.student_name AND b.student_name != c.student_name;
