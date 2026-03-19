c6483057d74f06dfc983e30b612ee75d53fb9656
Xcution <ghosh1k4@gmail.com>
2026-03-19 22:59:35 +0530

    Add solution and README entry for problem 1689

---
+++ Python/1689_Partitioning_Into_Minimum_Number_Of_Deci_Binary_Numbers.py
@@ -0,0 +1,10 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/
+
+class Solution(object):
+    def minPartitions(self, n):
+        """
+        :type n: str
+        :rtype: int
+        """
+        return int(max(n))
