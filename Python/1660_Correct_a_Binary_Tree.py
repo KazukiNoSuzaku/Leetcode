ae00546c7d20be0397495222f6c2e3873017eb07
Xcution <ghosh1k4@gmail.com>
2026-03-19 22:51:08 +0530

    Add solution and README entry for problem 1660

---
+++ Python/1660_Correct_a_Binary_Tree.py
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/correct-a-binary-tree/
+# Premium Problem
+# BFS right to left, remove node whose right child was already visited
+
+class Solution(object):
+    pass
