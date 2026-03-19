e52b1be881953bbbc751c56f5c03eae9a87b0348
Xcution <ghosh1k4@gmail.com>
2026-03-19 22:40:37 +0530

    Add solution and README entry for problem 1628

---
+++ Python/1628_Design_an_Expression_Tree_With_Evaluate_Function.py
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/design-an-expression-tree-with-evaluate-function/
+# Premium Problem
+# Build expression tree from postfix, evaluate recursively
+
+class Solution(object):
+    pass
