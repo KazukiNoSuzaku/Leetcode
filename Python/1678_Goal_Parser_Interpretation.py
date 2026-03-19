302c22f41be7ba7c33b9c28d61c20276c0c60428
Xcution <ghosh1k4@gmail.com>
2026-03-19 22:56:30 +0530

    Add solution and README entry for problem 1678

---
+++ Python/1678_Goal_Parser_Interpretation.py
@@ -0,0 +1,10 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/goal-parser-interpretation/
+
+class Solution(object):
+    def interpret(self, command):
+        """
+        :type command: str
+        :rtype: str
+        """
+        return command.replace('()', 'o').replace('(al)', 'al')
