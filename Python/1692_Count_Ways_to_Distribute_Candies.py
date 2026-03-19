46661e496ee9fcca7418f515639754970fe8061e
Xcution <ghosh1k4@gmail.com>
2026-03-19 23:00:25 +0530

    Add solution and README entry for problem 1692

---
+++ Python/1692_Count_Ways_to_Distribute_Candies.py
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/count-ways-to-distribute-candies/
+# Premium Problem
+# DP: dp[i][j] = ways to distribute i candies to j bags
+
+class Solution(object):
+    pass
