98ccf3cd551878c65f006a8435cf70789fbee2ac
Xcution <ghosh1k4@gmail.com>
2026-03-19 22:57:34 +0530

    Add solution and README entry for problem 1682

---
+++ Python/1682_Longest_Palindromic_Subsequence_II.py
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/longest-palindromic-subsequence-ii/
+# Premium Problem
+# DP with constraint: no two adjacent chars in palindrome are same
+
+class Solution(object):
+    pass
