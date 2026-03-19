466561861d15fc5b561efaca79590ab5aa0d9dc3
Xcution <ghosh1k4@gmail.com>
2026-03-19 22:36:42 +0530

    Add solution and README entry for problem 1612

---
+++ Python/1612_Check_If_Two_Expression_Trees_are_Equivalent.py
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/check-if-two-expression-trees-are-equivalent/
+# Premium Problem
+# Count frequency of each variable in both trees, compare
+
+class Solution(object):
+    pass
