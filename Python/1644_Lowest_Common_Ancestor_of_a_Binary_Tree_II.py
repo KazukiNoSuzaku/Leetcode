619f18262291183c52cdfed7628226edd8a9490c
Xcution <ghosh1k4@gmail.com>
2026-03-19 22:45:39 +0530

    Add solution and README entry for problem 1644

---
+++ Python/1644_Lowest_Common_Ancestor_of_a_Binary_Tree_II.py
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree-ii/
+# Premium Problem
+# Standard LCA but verify both p and q exist in the tree first
+
+class Solution(object):
+    pass
