15b0a9fdd1be51e73799079ca99516248b6de9d9
Xcution <ghosh1k4@gmail.com>
2026-03-19 22:55:48 +0530

    Add solution and README entry for problem 1676

---
+++ Python/1676_Lowest_Common_Ancestor_of_a_Binary_Tree_IV.py
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree-iv/
+# Premium Problem
+# DFS: if current node is in set, return it; merge results from left and right
+
+class Solution(object):
+    pass
