4c10af3c139b46ef09ffed537a3f975ee8dcca45
Xcution <ghosh1k4@gmail.com>
2026-03-19 22:45:05 +0530

    Add solution and README entry for problem 1642

---
+++ Python/1642_Furthest_Building_You_Can_Reach.py
@@ -0,0 +1,24 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/furthest-building-you-can-reach/
+
+import heapq
+
+class Solution(object):
+    def furthestBuilding(self, heights, bricks, ladders):
+        """
+        :type heights: List[int]
+        :type bricks: int
+        :type ladders: int
+        :rtype: int
+        """
+        heap = []  # min-heap of ladder climbs
+        for i in range(len(heights) - 1):
+            diff = heights[i + 1] - heights[i]
+            if diff <= 0:
+                continue
+            heapq.heappush(heap, diff)
+            if len(heap) > ladders:
+                bricks -= heapq.heappop(heap)
+            if bricks < 0:
+                return i
+        return len(heights) - 1
