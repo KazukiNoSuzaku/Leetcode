a422177318bf6dd6298aaae4833c3b290da9cfa6
Xcution <ghosh1k4@gmail.com>
2026-03-19 22:53:18 +0530

    Add solution and README entry for problem 1667

---
+++ SQL/1667_Fix_Names_in_a_Table.sql
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/fix-names-in-a-table/
+
+SELECT user_id,
+       CONCAT(UPPER(LEFT(name, 1)), LOWER(SUBSTRING(name, 2))) AS name
+FROM Users
+ORDER BY user_id;
