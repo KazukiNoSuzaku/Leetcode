56af1c8aa4f6f8c1848c064e217c072f552b278d
Xcution <ghosh1k4@gmail.com>
2026-03-19 22:47:53 +0530

    Add solution and README entry for problem 1651

---
+++ Python/1651_Hopper_Company_Queries_III.py
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/hopper-company-queries-iii/
+# Premium SQL Problem
+# 3-month moving average of ride distance and duration
+
+class Solution(object):
+    pass
