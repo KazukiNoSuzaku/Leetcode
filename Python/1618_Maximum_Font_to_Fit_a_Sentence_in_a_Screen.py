ffc02556bc13c5cd815042600e86d443616b06dd
Xcution <ghosh1k4@gmail.com>
2026-03-19 22:38:10 +0530

    Add solution and README entry for problem 1618

---
+++ Python/1618_Maximum_Font_to_Fit_a_Sentence_in_a_Screen.py
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/maximum-font-to-fit-a-sentence-in-a-screen/
+# Premium Problem
+# Binary search on font sizes, use FontInfo API to check if text fits
+
+class Solution(object):
+    pass
