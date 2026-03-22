94a4018191eecfe5f6bdf7766203bcefe3870f61
Xcution <ghosh1k4@gmail.com>
2026-03-23 03:20:15 +0530

    Add solution and README entry for problem 1778

---
+++ Python/1778_Shortest_Path_in_a_Hidden_Grid.py
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# Premium problem - Shortest Path in a Hidden Grid
+# Interactive problem: use GridMaster API to explore grid via DFS,
+# then BFS from start to target on discovered grid.
+
+class Solution(object):
+    pass
