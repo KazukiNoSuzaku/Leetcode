709286df172483b5aec27d1d32dcb6bce12218a6
Xcution <ghosh1k4@gmail.com>
2026-03-23 02:58:11 +0530

    Add solution and README entry for problem 1731

---
+++ Python/1731_The_Number_of_Employees_Which_Report_to_Each_Employee.py
@@ -0,0 +1,12 @@
+# Author: Kaustav Ghosh
+# SQL problem - The Number of Employees Which Report to Each Employee
+# SELECT e1.employee_id, e1.name,
+#        COUNT(e2.employee_id) AS reports_count,
+#        ROUND(AVG(e2.age)) AS average_age
+# FROM Employees e1
+# JOIN Employees e2 ON e1.employee_id = e2.reports_to
+# GROUP BY e1.employee_id, e1.name
+# ORDER BY e1.employee_id;
+
+class Solution(object):
+    pass
