e7a6ad7385dc5152541507ef1015af1fa94204b9
Xcution <ghosh1k4@gmail.com>
2026-03-23 03:02:31 +0530

    Add solution and README entry for problem 1741

---
+++ Python/1741_Find_Total_Time_Spent_by_Each_Employee.py
@@ -0,0 +1,9 @@
+# Author: Kaustav Ghosh
+# SQL problem - Find Total Time Spent by Each Employee
+# SELECT event_day AS day, emp_id,
+#        SUM(out_time - in_time) AS total_time
+# FROM Employees
+# GROUP BY event_day, emp_id;
+
+class Solution(object):
+    pass
