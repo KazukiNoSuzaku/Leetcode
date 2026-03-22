3f4a209223945b5b90f2891d236ca366573f5bd2
Xcution <ghosh1k4@gmail.com>
2026-03-23 02:52:37 +0530

    Add solution and README entry for problem 1724

---
+++ Python/1724_Checking_Existence_of_Edge_Length_Limited_Paths_II.py
@@ -0,0 +1,31 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/checking-existence-of-edge-length-limited-paths-ii/
+# Premium Problem
+#
+# Given an undirected weighted graph and online queries [u, v, limit],
+# determine if there exists a path from u to v where every edge weight
+# is strictly less than limit. Use offline-style persistent Union-Find
+# or a minimum spanning tree with binary lifting for online queries.
+#
+# class Solution(object):
+#     def __init__(self, n, edgeList):
+#         """
+#         :type n: int
+#         :type edgeList: List[List[int]]
+#         """
+#         # Sort edges by weight, build MST with binary lifting
+#         # for answering max-edge-weight queries on tree path
+#         pass
+#
+#     def query(self, p, q, limit):
+#         """
+#         :type p: int
+#         :type q: int
+#         :type limit: int
+#         :rtype: bool
+#         """
+#         # Check if max edge weight on path p->q in MST < limit
+#         pass
+
+class Solution(object):
+    pass
