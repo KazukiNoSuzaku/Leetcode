fc72ab1389f4b41c40ab504ede840a8e671501dc
Xcution <ghosh1k4@gmail.com>
2026-03-23 03:35:34 +0530

    Add solution and README entry for problem 1811

---
+++ Python/1811_Find_Interview_Candidates.py
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# Premium problem - Find Interview Candidates
+# SQL: Find users who won gold 3+ times or won consecutive contests.
+# Complex query with UNION of gold medal winners and consecutive contest winners.
+
+class Solution(object):
+    pass
