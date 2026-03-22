7072e54891dc062a3f7423e4c15eee280399ca2d
Xcution <ghosh1k4@gmail.com>
2026-03-23 03:37:57 +0530

    Add solution and README entry for problem 1816

---
+++ Python/1816_Truncate_Sentence.py
@@ -0,0 +1,10 @@
+# Author: Kaustav Ghosh
+
+class Solution(object):
+    def truncateSentence(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        return ' '.join(s.split()[:k])
