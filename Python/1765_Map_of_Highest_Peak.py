d27d504b11aadf16cda3fc8549b04275a9191248
Xcution <ghosh1k4@gmail.com>
2026-03-23 03:14:29 +0530

    Add solution and README entry for problem 1765

---
+++ Python/1765_Map_of_Highest_Peak.py
@@ -0,0 +1,25 @@
+# Author: Kaustav Ghosh
+
+class Solution(object):
+    def highestPeak(self, isWater):
+        """
+        :type isWater: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        from collections import deque
+        m, n = len(isWater), len(isWater[0])
+        height = [[-1] * n for _ in range(m)]
+        queue = deque()
+        for i in range(m):
+            for j in range(n):
+                if isWater[i][j] == 1:
+                    height[i][j] = 0
+                    queue.append((i, j))
+        while queue:
+            x, y = queue.popleft()
+            for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < m and 0 <= ny < n and height[nx][ny] == -1:
+                    height[nx][ny] = height[x][y] + 1
+                    queue.append((nx, ny))
+        return height
