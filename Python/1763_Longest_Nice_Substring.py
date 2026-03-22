4afed544d86f4a0263b5e16b46cde8a1e44bb270
Xcution <ghosh1k4@gmail.com>
2026-03-23 03:13:36 +0530

    Add solution and README entry for problem 1763

---
+++ Python/1763_Longest_Nice_Substring.py
@@ -0,0 +1,17 @@
+# Author: Kaustav Ghosh
+
+class Solution(object):
+    def longestNiceSubstring(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if len(s) < 2:
+            return ""
+        char_set = set(s)
+        for i, c in enumerate(s):
+            if c.swapcase() not in char_set:
+                left = self.longestNiceSubstring(s[:i])
+                right = self.longestNiceSubstring(s[i + 1:])
+                return left if len(left) >= len(right) else right
+        return s
