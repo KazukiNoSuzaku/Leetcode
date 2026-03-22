a6148e38b2c4e81daa34a5e80c2a7544ea745d9e
Xcution <ghosh1k4@gmail.com>
2026-03-23 03:35:01 +0530

    Add solution and README entry for problem 1810

---
+++ Python/1810_Minimum_Path_Cost_in_a_Hidden_Grid.py
@@ -0,0 +1,6 @@
+# Author: Kaustav Ghosh
+# Premium problem - Minimum Path Cost in a Hidden Grid
+# Interactive: explore grid via GridMaster API, then Dijkstra from start to target.
+
+class Solution(object):
+    pass
