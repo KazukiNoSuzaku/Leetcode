e1ee2b50d60f9dc6626414efc23c7bb55409c959
Xcution <ghosh1k4@gmail.com>
2026-03-23 03:42:54 +0530

    Add solution and README entry for problem 1826

---
+++ Python/1826_Faulty_Sensor.py
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# Premium problem - Faulty Sensor
+# Compare two sensor arrays to determine which one dropped a value.
+# Find first mismatch then check which shift pattern is consistent.
+
+class Solution(object):
+    pass
