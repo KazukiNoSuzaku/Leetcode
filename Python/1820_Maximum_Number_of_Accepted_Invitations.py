a03423e6ace6f158ece1b7a34a45f1dbd34b4f31
Xcution <ghosh1k4@gmail.com>
2026-03-23 03:39:55 +0530

    Add solution and README entry for problem 1820

---
+++ Python/1820_Maximum_Number_of_Accepted_Invitations.py
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# Premium problem - Maximum Number of Accepted Invitations
+# Bipartite matching: boys invite girls, maximize accepted invitations.
+# Use Hungarian algorithm / augmenting paths.
+
+class Solution(object):
+    pass
