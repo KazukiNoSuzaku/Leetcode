86f9cbfb57cebf3137cfd88b8deee8f45c466d3d
Xcution <ghosh1k4@gmail.com>
2026-03-23 03:23:00 +0530

    Add solution and README entry for problem 1784

---
+++ Python/1784_Check_if_Binary_String_Has_at_Most_One_Segment_of_Ones.py
@@ -0,0 +1,9 @@
+# Author: Kaustav Ghosh
+
+class Solution(object):
+    def checkOnesSegment(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        return '01' not in s
