f769e5fb42c3f14fdcc78d96d1e4362e02faf611
Xcution <ghosh1k4@gmail.com>
2026-03-23 03:40:26 +0530

    Add solution and README entry for problem 1821

---
+++ Python/1821_Find_Customers_With_Positive_Revenue_This_Year.py
@@ -0,0 +1,6 @@
+# Author: Kaustav Ghosh
+# Premium problem - Find Customers With Positive Revenue this Year
+# SQL: SELECT customer_id FROM Customers WHERE year = 2021 AND revenue > 0;
+
+class Solution(object):
+    pass
