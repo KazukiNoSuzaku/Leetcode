7705a943036293f2e1c29c15ce2274518d511e2c
Xcution <ghosh1k4@gmail.com>
2026-03-23 03:27:26 +0530

    Add solution and README entry for problem 1794

---
+++ Python/1794_Count_Pairs_of_Equal_Substrings_With_Minimum_Difference.py
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# Premium problem - Count Pairs of Equal Substrings With Minimum Difference
+# For each char, find leftmost in firstString and rightmost in secondString.
+# Track minimum (a-b) difference and count such pairs.
+
+class Solution(object):
+    pass
