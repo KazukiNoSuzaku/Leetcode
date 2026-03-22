2afd2aaf7aa4172dbb9bc1d174ab869a97fb4c21
Xcution <ghosh1k4@gmail.com>
2026-03-23 03:32:09 +0530

    Add solution and README entry for problem 1804

---
+++ Python/1804_Implement_Trie_II.py
@@ -0,0 +1,6 @@
+# Author: Kaustav Ghosh
+# Premium problem - Implement Trie II (Prefix Tree)
+# Trie with insert, countWordsEqualTo, countWordsStartingWith, and erase.
+
+class Solution(object):
+    pass
