faa30676bb1747658c553e23aff8c39ecfb37b4f
Xcution <ghosh1k4@gmail.com>
2026-03-23 03:04:51 +0530

    Add solution and README entry for problem 1746

---
+++ Python/1746_Maximum_Subarray_Sum_After_One_Operation.py
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# Premium problem - Maximum Subarray Sum After One Operation
+# Find max subarray sum where you can square at most one element.
+# Use DP with two states: squared used or not yet used.
+
+class Solution(object):
+    pass
