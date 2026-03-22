c9494cf90e680b17e8d064dc25c08d6daafee1e7
Xcution <ghosh1k4@gmail.com>
2026-03-23 03:17:40 +0530

    Add solution and README entry for problem 1772

---
+++ Python/1772_Sort_Features_by_Popularity.py
@@ -0,0 +1,6 @@
+# Author: Kaustav Ghosh
+# Premium problem - Sort Features by Popularity
+# Sort features by how many responses mention them, keeping original order for ties.
+
+class Solution(object):
+    pass
