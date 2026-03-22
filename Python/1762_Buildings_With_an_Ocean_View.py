ccd2b791c4566fd70cc1e446afcb6a5d0bf86212
Xcution <ghosh1k4@gmail.com>
2026-03-23 03:13:09 +0530

    Add solution and README entry for problem 1762

---
+++ Python/1762_Buildings_With_an_Ocean_View.py
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# Premium problem - Buildings With an Ocean View
+# Return indices of buildings that have an ocean view (no taller building to the right).
+# Traverse from right, track max height seen.
+
+class Solution(object):
+    pass
