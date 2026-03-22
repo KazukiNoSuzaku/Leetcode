173c61d718ab1c450b240d8d9457ef343056d756
Xcution <ghosh1k4@gmail.com>
2026-03-23 03:24:42 +0530

    Add solution and README entry for problem 1788

---
+++ Python/1788_Maximize_the_Beauty_of_the_Garden.py
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# Premium problem - Maximize the Beauty of the Garden
+# Find max sum subarray between matching flower values, including negatives.
+# Use prefix sums and hash map for first occurrence of each flower value.
+
+class Solution(object):
+    pass
