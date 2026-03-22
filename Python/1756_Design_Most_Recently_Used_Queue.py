5fb932882a5fd12babcb5ccd59fee0ac0b336951
Xcution <ghosh1k4@gmail.com>
2026-03-23 03:10:36 +0530

    Add solution and README entry for problem 1756

---
+++ Python/1756_Design_Most_Recently_Used_Queue.py
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# Premium problem - Design Most Recently Used Queue
+# Design a queue-like data structure that moves accessed element to end.
+# Use a list with fetch(k) that removes kth element and appends to end.
+
+class Solution(object):
+    pass
