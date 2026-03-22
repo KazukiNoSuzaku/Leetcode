1c62edc2a73a73688fa1ab10b1fe50ad511f3a1f
Xcution <ghosh1k4@gmail.com>
2026-03-23 03:02:06 +0530

    Add solution and README entry for problem 1740

---
+++ Python/1740_Find_Distance_in_a_Binary_Tree.py
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# Premium problem - Find Distance in a Binary Tree
+# Find the distance between two nodes p and q in a binary tree.
+# Use LCA (Lowest Common Ancestor) then sum distances from LCA to both nodes.
+
+class Solution(object):
+    pass
