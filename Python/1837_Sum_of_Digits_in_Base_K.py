df4b3138ff9ede41d935e8db1092658738640835
Xcution <ghosh1k4@gmail.com>
2026-03-24 01:52:56 +0530

    Add solution and README entry for problem 1837

---
+++ Python/1837_Sum_of_Digits_in_Base_K.py
@@ -0,0 +1,15 @@
+# Author: Kaustav Ghosh
+# Problem 1837: Sum of Digits in Base K
+
+class Solution(object):
+    def sumBase(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        total = 0
+        while n > 0:
+            total += n % k
+            n //= k
+        return total
