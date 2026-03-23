8cf7eb5dbed4838688532287e0ff7c5d8b163e1a
Xcution <ghosh1k4@gmail.com>
2026-03-24 01:55:25 +0530

    Add solution and README entry for problem 1846

---
+++ Python/1846_Maximum_Element_After_Decreasing_and_Rearranging.py
@@ -0,0 +1,15 @@
+# Author: Kaustav Ghosh
+# Problem 1846: Maximum Element After Decreasing and Rearranging
+
+class Solution(object):
+    def maximumElementAfterDecrementingAndRearranging(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        arr.sort()
+        arr[0] = 1
+        for i in range(1, len(arr)):
+            if arr[i] - arr[i - 1] > 1:
+                arr[i] = arr[i - 1] + 1
+        return arr[-1]
