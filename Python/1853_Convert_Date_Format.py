8c4c32fed5d5bd5dbfd9c03c11baf8748a2035a4
Xcution <ghosh1k4@gmail.com>
2026-03-24 01:57:18 +0530

    Add solution and README entry for problem 1853

---
+++ Python/1853_Convert_Date_Format.py
@@ -0,0 +1,9 @@
+# Author: Kaustav Ghosh
+# Problem 1853: Convert Date Format (Premium)
+# SQL Problem - Solution in comments
+#
+# SELECT DATE_FORMAT(day, '%W, %M %e, %Y') AS day
+# FROM Days;
+
+class Solution(object):
+    pass
