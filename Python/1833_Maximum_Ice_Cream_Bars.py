8e6ed9e907286140b33f018eddae998ee5ed76e0
Xcution <ghosh1k4@gmail.com>
2026-03-24 01:51:57 +0530

    Add solution and README entry for problem 1833

---
+++ Python/1833_Maximum_Ice_Cream_Bars.py
@@ -0,0 +1,19 @@
+# Author: Kaustav Ghosh
+# Problem 1833: Maximum Ice Cream Bars
+
+class Solution(object):
+    def maxIceCream(self, costs, coins):
+        """
+        :type costs: List[int]
+        :type coins: int
+        :rtype: int
+        """
+        costs.sort()
+        count = 0
+        for cost in costs:
+            if coins >= cost:
+                coins -= cost
+                count += 1
+            else:
+                break
+        return count
