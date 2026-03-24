81efb5e201c8bba31a286a09ab974ea0bec8e2cb
Xcution <ghosh1k4@gmail.com>
2026-03-25 00:13:21 +0530

    Add solution and README entry for problem 1885

---
+++ Python/1885_Count_Pairs_in_Two_Arrays.py
@@ -0,0 +1,10 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/count-pairs-in-two-arrays/
+# Premium problem
+# Given two integer arrays nums1 and nums2 of length n, count pairs (i, j)
+# where i < j and nums1[i] + nums1[j] > nums2[i] + nums2[j].
+# Rewrite as diff[i] + diff[j] > 0 where diff[i] = nums1[i] - nums2[i].
+# Sort diff, use two pointers.
+
+class Solution(object):
+    pass
