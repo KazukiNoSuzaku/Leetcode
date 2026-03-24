c724d399f9696db05f7d0d6956fd5e9243ec55b4
Xcution <ghosh1k4@gmail.com>
2026-03-25 00:24:15 +0530

    Add solution and README entry for problem 1924

---
+++ Python/1924_Erect_the_Fence_II.py
@@ -0,0 +1,8 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/erect-the-fence-ii/
+# Premium problem
+# Find the minimum enclosing circle for a set of points.
+# Use Welzl's algorithm for smallest enclosing circle.
+
+class Solution(object):
+    pass
