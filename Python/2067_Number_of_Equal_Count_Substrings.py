81ec60b0e96a9138fa5325b29dbe0d03895c0ed9
Xcution <ghosh1k4@gmail.com>
2026-03-25 01:13:50 +0530

    Add solution and README entry for problem 2067

---
+++ Python/2067_Number_of_Equal_Count_Substrings.py
@@ -0,0 +1,8 @@
+# Author: Kaustav Ghosh
+# Problem 2067: Number of Equal Count Substrings (Premium)
+# Given string s and integer count, return the number of substrings where
+# each unique character appears exactly count times.
+# Use sliding window for each possible number of distinct characters.
+
+class Solution(object):
+    pass
