a7e908640e4076ede140c9ca0e1fb6585fc89adc
Xcution <ghosh1k4@gmail.com>
2026-03-25 01:06:44 +0530

    Add solution and README entry for problem 2046

---
+++ Python/2046_Sort_Linked_List_Already_Sorted_Using_Absolute_Values.py
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# Problem 2046: Sort Linked List Already Sorted Using Absolute Values (Premium)
+# Given a linked list sorted by absolute values, sort it in actual order.
+# Move negative nodes to front by reversing their position.
+
+class Solution(object):
+    pass
