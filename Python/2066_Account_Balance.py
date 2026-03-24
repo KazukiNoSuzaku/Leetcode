abb63ded2dfd2a55b74dd12f3112d2a08f8c51ae
Xcution <ghosh1k4@gmail.com>
2026-03-25 01:13:30 +0530

    Add solution and README entry for problem 2066

---
+++ Python/2066_Account_Balance.py
@@ -0,0 +1,11 @@
+# Author: Kaustav Ghosh
+# Problem 2066: Account Balance (Premium)
+# SQL Problem:
+# SELECT account_id, day,
+#   SUM(CASE WHEN type = 'Deposit' THEN amount ELSE -amount END)
+#     OVER (PARTITION BY account_id ORDER BY day) AS balance
+# FROM Transactions
+# ORDER BY account_id, day;
+
+class Solution(object):
+    pass
