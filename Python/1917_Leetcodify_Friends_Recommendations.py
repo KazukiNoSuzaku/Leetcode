74f01ce67d6bc61bbbf2d0ae2b3912d2698f07d0
Xcution <ghosh1k4@gmail.com>
2026-03-25 00:22:39 +0530

    Add solution and README entry for problem 1917

---
+++ Python/1917_Leetcodify_Friends_Recommendations.py
@@ -0,0 +1,8 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/leetcodify-friends-recommendations/
+# Premium SQL problem
+# Recommend users who listened to the same 3+ songs on the same day
+# but are not already friends.
+
+class Solution(object):
+    pass
