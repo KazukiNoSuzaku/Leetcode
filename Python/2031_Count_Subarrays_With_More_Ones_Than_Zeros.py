effa1abd375306991f59c2f7a958b59193f657ff
Xcution <ghosh1k4@gmail.com>
2026-03-25 01:00:59 +0530

    Add solution and README entry for problem 2031

---
+++ Python/2031_Count_Subarrays_With_More_Ones_Than_Zeros.py
@@ -0,0 +1,8 @@
+# Author: Kaustav Ghosh
+# Problem 2031: Count Subarrays With More Ones Than Zeros (Premium)
+# Given a binary array nums, return the number of subarrays where
+# the count of ones is strictly greater than the count of zeros.
+# Use prefix sums and BIT to count valid subarrays.
+
+class Solution(object):
+    pass
