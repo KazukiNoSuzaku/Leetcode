7919e1f3194cda8d254066cb0a2e760932156420
Xcution <ghosh1k4@gmail.com>
2026-03-25 00:31:19 +0530

    Add solution and README entry for problem 1950

---
+++ Python/1950_Maximum_of_Minimum_Values_in_All_Subarrays.py
@@ -0,0 +1,8 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/maximum-of-minimum-values-in-all-subarrays/
+# Premium problem
+# For each element, find the max window size where it's the minimum.
+# Use monotonic stack to find previous and next smaller elements.
+
+class Solution(object):
+    pass
