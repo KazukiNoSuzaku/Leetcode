38ca57fdda82c34a883d9b6455570009b1bf7462
Xcution <ghosh1k4@gmail.com>
2026-03-25 00:33:04 +0530

    Add solution and README entry for problem 1956

---
+++ Python/1956_Minimum_Time_For_K_Virus_Variants_to_Spread.py
@@ -0,0 +1,8 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/minimum-time-for-k-virus-variants-to-spread/
+# Premium problem
+# Binary search on time t. For each t, check if any point is within
+# Manhattan distance t of at least k virus sources.
+
+class Solution(object):
+    pass
