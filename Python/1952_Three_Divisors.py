bc2103d86bfc46cff2a35196bc09df7c5c258b05
Xcution <ghosh1k4@gmail.com>
2026-03-25 00:31:49 +0530

    Add solution and README entry for problem 1952

---
+++ Python/1952_Three_Divisors.py
@@ -0,0 +1,14 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/three-divisors/
+
+class Solution(object):
+    def isThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        count = 0
+        for i in range(1, n + 1):
+            if n % i == 0:
+                count += 1
+        return count == 3
