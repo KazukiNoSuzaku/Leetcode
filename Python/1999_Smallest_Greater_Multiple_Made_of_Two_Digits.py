d7c0d507dc4e947410feac6ecaae87bec2740980
Xcution <ghosh1k4@gmail.com>
2026-03-25 00:46:34 +0530

    Add solution and README entry for problem 1999

---
+++ Python/1999_Smallest_Greater_Multiple_Made_of_Two_Digits.py
@@ -0,0 +1,9 @@
+# Author: Kaustav Ghosh
+# Problem 1999: Smallest Greater Multiple Made of Two Digits (Premium)
+#
+# Given k, digit1, digit2, find the smallest integer > k that is
+# composed only of digit1 and/or digit2. BFS/brute force generating
+# numbers from the two digits in increasing order.
+
+class Solution(object):
+    pass
