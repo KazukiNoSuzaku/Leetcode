00da46a6ea1aabb526bf52b0b7adcf562c4fa607
Xcution <ghosh1k4@gmail.com>
2026-03-25 00:37:46 +0530

    Add solution and README entry for problem 1972

---
+++ Python/1972_First_and_Last_Call_On_the_Same_Day.py
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/first-and-last-call-on-the-same-day/
+# Premium SQL problem
+# Find users whose first and last calls on the same day were with the same person.
+
+class Solution(object):
+    pass
