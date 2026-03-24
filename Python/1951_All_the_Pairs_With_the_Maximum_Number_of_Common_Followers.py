5cd78c0d428a360970ff449943a3d414de4d65f5
Xcution <ghosh1k4@gmail.com>
2026-03-25 00:31:35 +0530

    Add solution and README entry for problem 1951

---
+++ Python/1951_All_the_Pairs_With_the_Maximum_Number_of_Common_Followers.py
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/all-the-pairs-with-the-maximum-number-of-common-followers/
+# Premium SQL problem
+# Self-join Relations table to count common followers for each user pair.
+
+class Solution(object):
+    pass
