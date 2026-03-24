eaa926db1aa36efaaa87a2898dc7c1bb583d928a
Xcution <ghosh1k4@gmail.com>
2026-03-25 01:17:07 +0530

    Add solution and README entry for problem 2077

---
+++ Python/2077_Paths_in_Maze_That_Lead_to_Same_Room.py
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# Problem 2077: Paths in Maze That Lead to Same Room (Premium)
+# Count the number of cycles of length 3 (triangles) in an undirected graph.
+# For each edge (u,v), count common neighbors to find triangles.
+
+class Solution(object):
+    pass
