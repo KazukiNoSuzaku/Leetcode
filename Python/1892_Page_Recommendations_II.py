a826356d1bbe820c0872873b3653f2a0b8063d7f
Xcution <ghosh1k4@gmail.com>
2026-03-25 00:15:07 +0530

    Add solution and README entry for problem 1892

---
+++ Python/1892_Page_Recommendations_II.py
@@ -0,0 +1,8 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/page-recommendations-ii/
+# Premium SQL problem
+# Join Friendship and Likes tables to recommend pages liked by friends
+# but not already liked by the user.
+
+class Solution(object):
+    pass
