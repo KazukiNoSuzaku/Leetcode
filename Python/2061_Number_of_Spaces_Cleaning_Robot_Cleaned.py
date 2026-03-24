4ea7f4632c47db8fa468302336b83a215c9eda36
Xcution <ghosh1k4@gmail.com>
2026-03-25 01:11:59 +0530

    Add solution and README entry for problem 2061

---
+++ Python/2061_Number_of_Spaces_Cleaning_Robot_Cleaned.py
@@ -0,0 +1,8 @@
+# Author: Kaustav Ghosh
+# Problem 2061: Number of Spaces Cleaning Robot Cleaned (Premium)
+# Simulate a robot cleaning a grid. The robot starts at (0,0) facing right.
+# It moves forward if possible, otherwise turns right 90 degrees.
+# Stop when it revisits a cell facing the same direction.
+
+class Solution(object):
+    pass
