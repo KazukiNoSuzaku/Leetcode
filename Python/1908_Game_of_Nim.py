2eea803501931a9cba0fe774515c93b7a825e042
Xcution <ghosh1k4@gmail.com>
2026-03-25 00:20:19 +0530

    Add solution and README entry for problem 1908

---
+++ Python/1908_Game_of_Nim.py
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/game-of-nim/
+# Premium problem
+# Classic Nim: first player wins if XOR of all piles is non-zero.
+
+class Solution(object):
+    pass
