f2bcf23fd053e613fef14eada774a61bd7cdb067
Xcution <ghosh1k4@gmail.com>
2026-03-25 00:46:49 +0530

    Add solution and README entry for problem 2000

---
+++ Python/2000_Reverse_Prefix_of_Word.py
@@ -0,0 +1,14 @@
+# Author: Kaustav Ghosh
+# Problem 2000: Reverse Prefix of Word
+
+class Solution(object):
+    def reversePrefix(self, word, ch):
+        """
+        :type word: str
+        :type ch: str
+        :rtype: str
+        """
+        idx = word.find(ch)
+        if idx == -1:
+            return word
+        return word[:idx + 1][::-1] + word[idx + 1:]
