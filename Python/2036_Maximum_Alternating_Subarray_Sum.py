9c5bc6090b1259f86ff32e9c23a1da2d0ecae958
Xcution <ghosh1k4@gmail.com>
2026-03-25 01:02:41 +0530

    Add solution and README entry for problem 2036

---
+++ Python/2036_Maximum_Alternating_Subarray_Sum.py
@@ -0,0 +1,8 @@
+# Author: Kaustav Ghosh
+# Problem 2036: Maximum Alternating Subarray Sum (Premium)
+# Given an array nums, return the maximum alternating subarray sum.
+# An alternating subarray sum is computed as nums[i] - nums[i+1] + nums[i+2] - ...
+# Use Kadane's style DP tracking positive and negative position sums.
+
+class Solution(object):
+    pass
