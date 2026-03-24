43266ee4e104d27de0c75eb4266d299e58d1fd39
Xcution <ghosh1k4@gmail.com>
2026-03-25 00:31:04 +0530

    Add solution and README entry for problem 1949

---
+++ Python/1949_Strong_Friendship.py
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/strong-friendship/
+# Premium SQL problem
+# Find pairs of friends who have at least 3 common friends.
+
+class Solution(object):
+    pass
