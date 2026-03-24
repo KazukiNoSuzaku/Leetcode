a39dbc2022d491ba14e1e1c2d4ab05f333c8cb21
Xcution <ghosh1k4@gmail.com>
2026-03-25 00:14:54 +0530

    Add solution and README entry for problem 1891

---
+++ Python/1891_Cutting_Ribbons.py
@@ -0,0 +1,8 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/cutting-ribbons/
+# Premium problem
+# Binary search on the answer: for a given length, count how many ribbons
+# can be cut. Find the maximum length that yields at least k ribbons.
+
+class Solution(object):
+    pass
