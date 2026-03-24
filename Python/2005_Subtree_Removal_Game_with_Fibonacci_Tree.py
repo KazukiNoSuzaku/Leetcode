e28b36a2b220e68ae240fe1a55592bfee906580d
Xcution <ghosh1k4@gmail.com>
2026-03-25 00:48:23 +0530

    Add solution and README entry for problem 2005

---
+++ Python/2005_Subtree_Removal_Game_with_Fibonacci_Tree.py
@@ -0,0 +1,9 @@
+# Author: Kaustav Ghosh
+# Problem 2005: Subtree Removal Game with Fibonacci Tree (Premium)
+#
+# A Fibonacci tree of order n: if n <= 2, single node; else root with
+# children fib(n-1) and fib(n-2). The first player wins iff n is not
+# divisible by 6 (based on Sprague-Grundy theory on Fibonacci trees).
+
+class Solution(object):
+    pass
