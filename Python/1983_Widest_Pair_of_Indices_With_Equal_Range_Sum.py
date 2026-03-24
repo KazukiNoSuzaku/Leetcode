15afbf959a3fcf390e6600384c18f9f5f1150e1a
Xcution <ghosh1k4@gmail.com>
2026-03-25 00:41:37 +0530

    Add solution and README entry for problem 1983

---
+++ Python/1983_Widest_Pair_of_Indices_With_Equal_Range_Sum.py
@@ -0,0 +1,10 @@
+# Author: Kaustav Ghosh
+# Problem 1983: Widest Pair of Indices With Equal Range Sum (Premium)
+#
+# SQL: N/A (Premium problem)
+# Given two binary arrays nums1 and nums2 of the same length, find the widest
+# pair of indices (i, j) where sum(nums1[i..j]) == sum(nums2[i..j]).
+# Use prefix sum difference and hash map to track first occurrence.
+
+class Solution(object):
+    pass
