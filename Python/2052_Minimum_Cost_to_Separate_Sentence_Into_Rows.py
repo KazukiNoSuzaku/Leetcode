1e34a680c6f9c213cd0dddc3115d38cd4f020239
Xcution <ghosh1k4@gmail.com>
2026-03-25 01:08:42 +0530

    Add solution and README entry for problem 2052

---
+++ Python/2052_Minimum_Cost_to_Separate_Sentence_Into_Rows.py
@@ -0,0 +1,8 @@
+# Author: Kaustav Ghosh
+# Problem 2052: Minimum Cost to Separate Sentence Into Rows (Premium)
+# Given a sentence and max width k, split into rows minimizing
+# the sum of squares of extra spaces (except last row).
+# Use DP where dp[i] = min cost to arrange words[i:] into rows.
+
+class Solution(object):
+    pass
