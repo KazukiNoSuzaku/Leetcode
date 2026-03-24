3215381ce7942215cde50b05ae34a163704a51fa
Xcution <ghosh1k4@gmail.com>
2026-03-25 00:22:52 +0530

    Add solution and README entry for problem 1918

---
+++ Python/1918_Kth_Smallest_Subarray_Sum.py
@@ -0,0 +1,8 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/kth-smallest-subarray-sum/
+# Premium problem
+# Binary search on the answer. For a given target sum, count how many
+# subarrays have sum <= target using sliding window.
+
+class Solution(object):
+    pass
