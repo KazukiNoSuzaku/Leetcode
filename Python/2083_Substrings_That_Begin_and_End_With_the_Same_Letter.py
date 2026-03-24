907703b2b00902029c360789d1aee8ba9666ff37
Xcution <ghosh1k4@gmail.com>
2026-03-25 01:37:42 +0530

    Add solution and README entry for problem 2083

---
+++ Python/2083_Substrings_That_Begin_and_End_With_the_Same_Letter.py
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# Problem 2083: Substrings That Begin and End With the Same Letter (Premium)
+# For each character, count occurrences so far. Each new occurrence of char c
+# that has been seen k times adds k+1 substrings (including single char).
+
+class Solution(object):
+    pass
