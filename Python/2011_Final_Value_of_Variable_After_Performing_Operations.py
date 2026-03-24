da4ed56930b66b81fe85cf704c210b960f434ef2
Xcution <ghosh1k4@gmail.com>
2026-03-25 00:49:59 +0530

    Add solution and README entry for problem 2011

---
+++ Python/2011_Final_Value_of_Variable_After_Performing_Operations.py
@@ -0,0 +1,16 @@
+# Author: Kaustav Ghosh
+# Problem 2011: Final Value of Variable After Performing Operations
+
+class Solution(object):
+    def finalValueAfterOperations(self, operations):
+        """
+        :type operations: List[str]
+        :rtype: int
+        """
+        x = 0
+        for op in operations:
+            if '+' in op:
+                x += 1
+            else:
+                x -= 1
+        return x
