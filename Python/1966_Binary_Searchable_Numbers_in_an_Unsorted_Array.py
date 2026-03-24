7e0bbbbd2acd717737321ffcf5589a7749cd6db1
Xcution <ghosh1k4@gmail.com>
2026-03-25 00:35:58 +0530

    Add solution and README entry for problem 1966

---
+++ Python/1966_Binary_Searchable_Numbers_in_an_Unsorted_Array.py
@@ -0,0 +1,9 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/binary-searchable-numbers-in-an-unsorted-array/
+# Premium problem
+# Count elements that would be found by binary search regardless of pivot choices.
+# An element is binary searchable if all elements to its left are smaller
+# and all elements to its right are larger.
+
+class Solution(object):
+    pass
