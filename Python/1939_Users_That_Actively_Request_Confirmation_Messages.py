1f56744c8e32536c5c7b4a0db8cb1494b0f83334
Xcution <ghosh1k4@gmail.com>
2026-03-25 00:28:22 +0530

    Add solution and README entry for problem 1939

---
+++ Python/1939_Users_That_Actively_Request_Confirmation_Messages.py
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/users-that-actively-request-confirmation-messages/
+# Premium SQL problem
+# Find users who requested two confirmation messages within a 24-hour window.
+
+class Solution(object):
+    pass
