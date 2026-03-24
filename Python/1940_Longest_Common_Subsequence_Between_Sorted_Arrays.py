0ed924a7a7ba85c927f237bfdceb2275e1a68f72
Xcution <ghosh1k4@gmail.com>
2026-03-25 00:28:38 +0530

    Add solution and README entry for problem 1940

---
+++ Python/1940_Longest_Common_Subsequence_Between_Sorted_Arrays.py
@@ -0,0 +1,8 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/longest-common-subsequence-between-sorted-arrays/
+# Premium problem
+# Find longest common subsequence among all sorted arrays.
+# Since arrays are sorted, find common elements across all arrays.
+
+class Solution(object):
+    pass
