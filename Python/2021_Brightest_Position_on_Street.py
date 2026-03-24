905277b7d9dd51d3e126f305026fdeafd2630773
Xcution <ghosh1k4@gmail.com>
2026-03-25 00:52:44 +0530

    Add solution and README entry for problem 2021

---
+++ Python/2021_Brightest_Position_on_Street.py
@@ -0,0 +1,9 @@
+# Author: Kaustav Ghosh
+# Problem 2021: Brightest Position on Street (Premium)
+#
+# Use a sweep line / difference array approach.
+# For each light at position p with range r, increment at p-r and decrement at p+r+1.
+# Scan left to right to find position with maximum brightness.
+
+class Solution(object):
+    pass
