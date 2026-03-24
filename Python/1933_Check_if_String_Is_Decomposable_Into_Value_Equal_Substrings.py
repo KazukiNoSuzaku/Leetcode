040a3f45bb03674ec66bb9c3db7593092a491a72
Xcution <ghosh1k4@gmail.com>
2026-03-25 00:26:36 +0530

    Add solution and README entry for problem 1933

---
+++ Python/1933_Check_if_String_Is_Decomposable_Into_Value_Equal_Substrings.py
@@ -0,0 +1,8 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/check-if-string-is-decomposable-into-value-equal-substrings/
+# Premium problem
+# Check if string can be split into substrings of length 3 with equal chars,
+# except exactly one substring of length 2.
+
+class Solution(object):
+    pass
