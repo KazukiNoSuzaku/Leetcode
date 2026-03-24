69cad16e09a5c19f51ec36749183e6aca416d070
Xcution <ghosh1k4@gmail.com>
2026-03-25 00:43:12 +0530

    Add solution and README entry for problem 1989

---
+++ Python/1989_Maximum_Number_of_People_That_Can_Be_Caught_in_Tag.py
@@ -0,0 +1,8 @@
+# Author: Kaustav Ghosh
+# Problem 1989: Maximum Number of People That Can Be Caught in Tag (Premium)
+#
+# Greedy approach: Sort the "it" players and runners.
+# For each "it" player, greedily match with the closest unmatched runner within dist.
+
+class Solution(object):
+    pass
