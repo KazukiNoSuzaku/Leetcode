44daea6e2c3a434deaab24769d1cf53819db7a05
Xcution <ghosh1k4@gmail.com>
2026-03-25 00:18:43 +0530

    Add solution and README entry for problem 1902

---
+++ Python/1902_Depth_of_BST_Given_Insertion_Order.py
@@ -0,0 +1,8 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/depth-of-bst-given-insertion-order/
+# Premium problem
+# Given insertion order of a BST, find its depth.
+# Use a sorted container to track inserted values and their depths.
+
+class Solution(object):
+    pass
