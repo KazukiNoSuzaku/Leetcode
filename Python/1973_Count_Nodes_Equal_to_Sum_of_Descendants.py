9da1203818a2971a55f2c789a88a0f5607c429e9
Xcution <ghosh1k4@gmail.com>
2026-03-25 00:37:59 +0530

    Add solution and README entry for problem 1973

---
+++ Python/1973_Count_Nodes_Equal_to_Sum_of_Descendants.py
@@ -0,0 +1,8 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/count-nodes-equal-to-sum-of-descendants/
+# Premium problem
+# Count nodes whose value equals the sum of all descendant values.
+# Use post-order traversal to compute subtree sums.
+
+class Solution(object):
+    pass
