2487de7ac6d898ea8ddff38550848d87228d683b
Xcution <ghosh1k4@gmail.com>
2026-03-25 00:14:41 +0530

    Add solution and README entry for problem 1890

---
+++ Python/1890_The_Latest_Login_in_2020.py
@@ -0,0 +1,10 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/the-latest-login-in-2020/
+# SQL problem
+# SELECT user_id, MAX(time_stamp) AS last_stamp
+# FROM Logins
+# WHERE YEAR(time_stamp) = 2020
+# GROUP BY user_id;
+
+class Solution(object):
+    pass
