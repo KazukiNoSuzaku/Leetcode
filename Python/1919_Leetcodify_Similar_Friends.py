334c707f3a8c03c3e82f3980c42c034b95e62678
Xcution <ghosh1k4@gmail.com>
2026-03-25 00:23:04 +0530

    Add solution and README entry for problem 1919

---
+++ Python/1919_Leetcodify_Similar_Friends.py
@@ -0,0 +1,7 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/leetcodify-similar-friends/
+# Premium SQL problem
+# Find pairs of friends who listened to the same 3+ distinct songs on the same day.
+
+class Solution(object):
+    pass
