a1e570fac2c3aa5589459c19f75a567eb6557a00
Xcution <ghosh1k4@gmail.com>
2026-03-28 03:58:51 +0530

    Add solution and README entry for problem 2102

---
+++ Python/2102_Sequentially_Ordinal_Rank_Tracker.py
@@ -0,0 +1,20 @@
+# Author: Kaustav Ghosh
+# https://leetcode.com/problems/sequentially-ordinal-rank-tracker/
+# Premium - Design problem using SortedList
+#
+# class SORTracker(object):
+#     def __init__(self):
+#         self.data = []  # sorted list of (-score, name)
+#         self.idx = 0
+#
+#     def add(self, name, score):
+#         import bisect
+#         bisect.insort(self.data, (-score, name))
+#
+#     def get(self):
+#         result = self.data[self.idx]
+#         self.idx += 1
+#         return result[1]
+
+class Solution(object):
+    pass
