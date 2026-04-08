bee8a6d8ecadccfc37dd5e5c971d11c229511707
Xcution <ghosh1k4@gmail.com>
2026-04-09 02:55:46 +0530

    Add solution and README entry for problem 2235

---
+++ Python/2235_Add_Two_Integers.py
@@ -0,0 +1,16 @@
+# Author: Kaustav Ghosh
+# Problem: 2235. Add Two Integers
+# URL: https://leetcode.com/problems/add-two-integers/
+# Difficulty: Easy
+#
+# Approach:
+# Simply return the sum of the two given integers.
+
+class Solution(object):
+    def sum(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        return num1 + num2
