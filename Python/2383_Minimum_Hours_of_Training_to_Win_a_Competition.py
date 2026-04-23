717d53cbb7ef3f1bac73e0949f6099c3cb363df9
Xcution <ghosh1k4@gmail.com>
2026-04-23 16:43:32 +0530

    Add solution and README entry for problem 2383

---
+++ Python/2383_Minimum_Hours_of_Training_to_Win_a_Competition.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minNumberOfHours(self, initialEnergy: int, initialExperience: int,
+                         energy: list[int], experience: list[int]) -> int:
+        hours = 0
+        e, exp = initialEnergy, initialExperience
+        for en, ex in zip(energy, experience):
+            if e <= en:
+                hours += en - e + 1
+                e = en + 1
+            if exp <= ex:
+                hours += ex - exp + 1
+                exp = ex + 1
+            e -= en
+            exp += ex
+        return hours
