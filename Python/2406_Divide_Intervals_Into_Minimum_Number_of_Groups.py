e56f4cbdb182e314f7a07331c2177b8502a8c42b
Xcution <ghosh1k4@gmail.com>
2026-04-25 00:17:21 +0530

    Add solution and README entry for problem 2406

---
+++ Python/2406_Divide_Intervals_Into_Minimum_Number_of_Groups.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minGroups(self, intervals: list[list[int]]) -> int:
+        events = []
+        for l, r in intervals:
+            events.append((l, 1))
+            events.append((r + 1, -1))
+        events.sort()
+        ans = cur = 0
+        for _, delta in events:
+            cur += delta
+            ans = max(ans, cur)
+        return ans
