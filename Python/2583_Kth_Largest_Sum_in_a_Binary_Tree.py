863e98de6b095d095ff72c39235ac3221d78b0fc
Xcution <ghosh1k4@gmail.com>
2026-05-17 04:08:14 +0530

    Add solution and README entry for problem 2583

---
+++ Python/2583_Kth_Largest_Sum_in_a_Binary_Tree.py
@@ -0,0 +1,28 @@
+from collections import deque
+from typing import Optional
+import heapq
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def kthLargestLevelSum(self, root: Optional[TreeNode], k: int) -> int:
+        level_sums = []
+        queue = deque([root])
+        while queue:
+            level_sum = 0
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                level_sum += node.val
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            level_sums.append(level_sum)
+        if k > len(level_sums):
+            return -1
+        level_sums.sort(reverse=True)
+        return level_sums[k - 1]
