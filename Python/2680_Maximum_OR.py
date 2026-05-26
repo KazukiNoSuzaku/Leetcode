a9fc78c2eebf4c1ae77b5d5c2d33841cf4d14b8c
Xcution <ghosh1k4@gmail.com>
2026-05-26 07:20:47 +0530

    Add solution and README entry for problem 2680

---
+++ Python/2680_Maximum_OR.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maximumOr(self, nums: list[int], k: int) -> int:
+        n = len(nums)
+        prefix = [0] * (n + 1)
+        suffix = [0] * (n + 1)
+        for i in range(n):
+            prefix[i + 1] = prefix[i] | nums[i]
+        for i in range(n - 1, -1, -1):
+            suffix[i] = suffix[i + 1] | nums[i]
+        return max(prefix[i] | (nums[i] << k) | suffix[i + 1] for i in range(n))
