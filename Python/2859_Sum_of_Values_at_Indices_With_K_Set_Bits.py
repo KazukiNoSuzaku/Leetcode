8a701b8874c3bf3c4dc63a25b370caaf07184fc0
Kaustav Ghosh <kaustavghosh@Kaustavs-MacBook-Air.local>
2026-06-08 23:54:00 +0530

    Add solution and README entry for problem 2859

---
+++ Python/2859_Sum_of_Values_at_Indices_With_K_Set_Bits.py
@@ -0,0 +1,5 @@
+from typing import List
+
+class Solution:
+    def sumIndicesWithKSetBits(self, nums: List[int], k: int) -> int:
+        return sum(nums[i] for i in range(len(nums)) if bin(i).count('1') == k)
