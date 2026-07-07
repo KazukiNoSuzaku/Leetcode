f2c5a4b2e187462b41c2179ea12a70d8b4d14a2b
Kaustav Ghosh <ghosh1k4@gmail.com>
2026-07-07 07:38:51 +0530

    Add solution for problem 1646: Get Maximum in Generated Array

---
+++ Python/1646_Get_Maximum_in_Generated_Array.py
@@ -0,0 +1,20 @@
+# Author: Kaustav Ghosh
+# Problem: Get Maximum in Generated Array
+# Approach: Build the array by its recurrence (even index copies nums[i/2], odd adds the next), tracking the running maximum
+
+class Solution(object):
+    def getMaximumGenerated(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 0:
+            return 0
+        nums = [0] * (n + 1)
+        nums[1] = 1
+        for i in range(2, n + 1):
+            if i % 2 == 0:
+                nums[i] = nums[i // 2]
+            else:
+                nums[i] = nums[i // 2] + nums[i // 2 + 1]
+        return max(nums)
