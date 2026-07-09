128beb8134d2663cfdf163d96fcd564713a5a836
Kaustav Ghosh <ghosh1k4@gmail.com>
2026-07-09 11:44:29 +0530

    Fix problem 1665: sort tasks ascending by (minimum - actual)

--- Python/1665_Minimum_Initial_Energy_to_Finish_Tasks.py
+++ Python/1665_Minimum_Initial_Energy_to_Finish_Tasks.py
@@ -1,6 +1,6 @@
 # Author: Kaustav Ghosh
 # Problem: Minimum Initial Energy to Finish Tasks
-# Approach: Do the tasks with the biggest (minimum - actual) buffer last; sweep keeping energy = max(energy + actual, minimum)
+# Approach: Sort by (minimum - actual) ascending; sweeping energy = max(energy + actual, minimum) then yields the minimum start energy (it effectively schedules the largest-buffer tasks first)
 
 class Solution(object):
     def minimumEffort(self, tasks):
@@ -8,7 +8,7 @@ class Solution(object):
         :type tasks: List[List[int]]
         :rtype: int
         """
-        tasks.sort(key=lambda t: t[1] - t[0], reverse=True)
+        tasks.sort(key=lambda t: t[1] - t[0])
         energy = 0
         for actual, minimum in tasks:
             energy = max(energy + actual, minimum)
